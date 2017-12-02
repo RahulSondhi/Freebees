8017e07d08924e6d1307fddf0ebe4133e24c0c7c
RahulSondhi <rahul.sondhi@hotmail.com>
2017-12-01 22:56:29 -0500

    Skeleton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+$(function() {
+  setSky();
+});
 
+function setSky() {
+  var time = new Date();
+  $("#stage").addClass("sky-gradient-"+time.getHours());
+  if (time.getHours() > 15 || time.getHours() < 8) {
+    $("body").css("background-color", "#505050");
+  } else {
+    $("body").css("background-color", "#000");
+  }
+  window.setTimeout(function() {
+      setSky();
+  }, 60000);
+}
